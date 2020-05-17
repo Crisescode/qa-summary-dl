63f1cd8702cff7fecf9dae8b593e057af55dfcce
Crisescode <zhaopanp2018@outlook.com>
2020-05-17 21:21:53 +0800

    update seq2seq model

--- py_project/utils/config.py
+++ py_project/utils/config.py
@@ -9,3 +9,8 @@ stop_words_path = os.path.join(root, "Hashing", "common_data", "stopwords", "哈
 train_x_path = os.path.join(root, "Hashing", "gen_data", "train_X")
 train_y_path = os.path.join(root, "Hashing", "gen_data", "train_Y")
 test_x_path = os.path.join(root, "Hashing", "gen_data", "test_X")
+
+save_wv_model_path = os.path.join(root, "gen_data", "word2vec.model")
+
+embedding_matrix_path = os.path.join(root, "gen_data", "embedding_matrix")
+
---
+++ py_project/utils/wv_loader.py
@@ -0,0 +1,17 @@
+import logging
+import numpy as np
+import codecs
+
+from gensim.models.word2vec import LineSentence, Word2Vec
+
+from .config import embedding_matrix_path
+
+
+logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
+
+
+def load_word2vec_file(save_wv_model_path):
+    pass
+
+
+
