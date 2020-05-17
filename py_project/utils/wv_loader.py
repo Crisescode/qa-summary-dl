2ac651fca2f5499ced24fe6fd640aaee1225a4ac
Crisescode <zhaopanp2018@outlook.com>
2020-05-17 21:35:49 +0800

    update seq2seq model

--- py_project/utils/wv_loader.py
+++ py_project/utils/wv_loader.py
@@ -1,6 +1,5 @@
 import logging
 import numpy as np
-import codecs
 
 from gensim.models.word2vec import LineSentence, Word2Vec
 
@@ -11,7 +10,34 @@ logging.basicConfig(format="%(asctime)s : %(levelname)s : %(message)s", level=lo
 
 
 def load_word2vec_file(save_wv_model_path):
-    pass
+    wv_model = Word2Vec.load(save_wv_model_path)
+    embedding_matrix = wv_model.wv.vectors
+
+    return embedding_matrix
+
+
+def get_vocab(save_wv_model_path):
+    wv_model = Word2Vec.load(save_wv_model_path)
+    vocab = {word: index for index, word in enumerate(wv_model.wv.index2word)}
+    reverse_vocab = {index:word for index, word in enumerate(wv_model.wv.index2word)}
+
+    return vocab, reverse_vocab
+
+
+def load_vocab(file_path):
+    vocab = {}
+    reverse_vocab = {}
+    for line in open(file_path, "r", encoding='utf-8').readlines():
+        word, index = line.strip().split("\t")
+        index = int(index)
+        vocab[word] = index
+        reverse_vocab[index] = word
+    return vocab, reverse_vocab
+
+
+def load_embedding_matrix():
+    return np.load(embedding_matrix_path + ".npy")
+
 
 
 
