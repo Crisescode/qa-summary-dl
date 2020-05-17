4e8dd98e9c1774fce2d3d8851909a86bad843da8
Crisescode <zhaopanp2018@outlook.com>
2020-05-17 15:32:28 +0800

    update utils

--- py_project/utils/config.py
+++ py_project/utils/config.py
@@ -3,6 +3,8 @@ import pathlib
 
 root = pathlib.Path(os.path.abspath(__file__))
 
+stop_words_path = os.path.join(root, "common_data", "stopwords", "哈工大停用词表.txt")
+
 train_x_path = os.path.join(root, "gen_data", "train_X")
 train_y_path = os.path.join(root, "gen_data", "train_Y")
 test_x_path = os.path.join(root, "gen_data", "test_X")
--- py_project/utils/data_loader.py
+++ py_project/utils/data_loader.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .config import train_x_path, train_y_path, test_x_path
+from .config import train_x_path, train_y_path, test_x_path, stop_words_path
 
 
 def load_dataset():
@@ -15,3 +15,65 @@ def load_dataset():
     test_X.dtype = "float64"
 
     return train_X, train_Y, test_X
+
+
+def load_stop_words(path):
+    with open(path, 'r', encoding="utf-8") as f:
+        stop_words = [word.strip() for word in f.readlines()]
+
+    return stop_words
+
+
+stop_words = load_stop_words(stop_words_path)
+
+
+def clean_sentence(sentence):
+    import re
+    if isinstance(sentence, str):
+        return re.sub(
+            r'[\s+\-\|\!\/\[\]\{\}_,.$%^*(+\"\')]+|[:：+——()?【】“”！，。？、~@#￥%……&*（）]+|车主说|技师说|语音|图片|你好|您好',
+            '', sentence)
+    else:
+        return ""
+
+
+def filter_stopwords(words):
+    return [word for word in words if word not in stop_words]
+
+
+def sentence_proc(sentence):
+    import jieba
+    sentence = clean_sentence(sentence)
+    words = jieba.cut(sentence)
+    words = filter_stopwords(words)
+
+    return " ".join(words)
+
+
+def pad_proc(sentence, max_len, vocab):
+    words = sentence.strip().split(" ")
+    words = words[:max_len]
+
+    sentence = [word if word in vocab else "<UNK>" for word in words]
+    sentence = ["<START>"] + sentence + ["<STOP>"]
+    sentence = sentence + ["<PAD>"] * (max_len - len(words))
+
+    return " ".join(sentence)
+
+
+def transform_data(sentence, vocab):
+    words = sentence.split(" ")
+    ids = [vocab[word] if word in vocab else vocab["<UNK>"] for word in words]
+
+    return ids
+
+
+def preprocess_sentence(sentence, max_len, vocab):
+    # 切词处理
+    sentence = sentence_proc(sentence)
+    # 填充处理
+    sentence = pad_proc(sentence, max_len, vocab)
+    # 数值化处理
+    sentence = transform_data(sentence, vocab)
+
+    return np.array([sentence])
